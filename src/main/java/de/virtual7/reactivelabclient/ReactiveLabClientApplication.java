4d22bf3e13896ed1a0aff0f65b66a16f9f933541
meehighd <mih.dobrescu@gmail.com>
2018-03-11 13:14:11 +0100

    First commit

---
+++ src/main/java/de/virtual7/reactivelabclient/ReactiveLabClientApplication.java
@@ -0,0 +1,12 @@
+package de.virtual7.reactivelabclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveLabClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveLabClientApplication.class, args);
+	}
+}
